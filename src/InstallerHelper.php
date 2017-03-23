a74aafeac7e27508d2bb7408db569ba18955dbd4
Anton Ukhanev <xedin.unknown@gmail.com>
2017-03-23 14:36:03 +0100

    Update InstallerHelper.php

--- src/InstallerHelper.php
+++ src/InstallerHelper.php
@@ -9,7 +9,7 @@ class InstallerHelper extends BaseInstaller {
 	function getLocations() {
 		// it will be looking for a key of FALSE, which evaluates to 0, i.e. the first element
 		// that element value being false signals the installer to use the default path
-		return [ false ];
+		return array( false );
 	}
 
 }
