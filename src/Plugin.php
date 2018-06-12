b9f779bb54723014514afee4e25e19d927422cbb
Nathan Dentzau <nathan.dentzau@gmail.com>
2018-06-12 00:07:43 -0400

    Refactor code
    * Implement PSR-2 coding standards
    * Use PHP 7 syntax
    * Make code easier to be unit tested

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,16 +1,22 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace OomphInc\ComposerInstallersExtender;
 
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use OomphInc\ComposerInstallersExtender\Installers\Installer;
 
-class Plugin implements PluginInterface {
-
-	public function activate( Composer $composer, IOInterface $io ) {
-		$installer = new Installer( $io, $composer );
-		$composer->getInstallationManager()->addInstaller( $installer );
-	}
-
+class Plugin implements PluginInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
 }
