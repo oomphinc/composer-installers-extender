71474fefeeec1cb20920947e1522d9518a5c84f8
James Gilliland <neclimdul@gmail.com>
2021-04-15 10:00:01 -0500

    Adjust namespace hack to support PHP 8

--- src/Installers/CustomInstaller.php
+++ src/Installers/CustomInstaller.php
@@ -17,9 +17,14 @@ class CustomInstaller extends BaseInstaller
 {
     public function getLocations()
     {
-        // It will be looking for a key of FALSE, which evaluates to 0, i.e. the
-        // first element that element value being false signals the installer to
-        // use the default path.
-        return [ false ];
+        /* In some cases where installers use 'library' or other non namespaced
+         * types composer will fail to handle the installer but this project is
+         * specifically supporting that case so this works around composer.
+         *
+         * In PHP 7.x composer will will be looking for a key of FALSE, which
+         * evaluates to 0. In PHP 8.x composer will be looking for a key of "".
+         * A value of false signals the installer to use the default path.
+         */
+        return [ 0 => false, '' => false ];
     }
 }
